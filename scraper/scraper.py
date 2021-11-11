555d479791471d12b5a6809f0b2191ef2affa2a0
Darcy Russell <darcy.russell@smartcaptech.com>
2021-11-11 19:07:17 +1000

    Added unittests and cleaned up code

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,13 +1,13 @@
 class Scraper:
     
-    def __init__(self, job_titles, locations, search_url, job_url, filters=None):
+    def __init__(self, job_titles, locations, search_url_tmplt, job_url_tmplt, filters=None):
         self.job_titles = job_titles
         self.locations = locations
-        self.filters = filters
-        self.search_url_template = search_url
-        self.job_url_template = job_url
-        
-        
+        self.search_url_template = search_url_tmplt
+        self.job_url_template = job_url_tmplt
+        self.filters = self.__decapitalise_filters(filters)
+    
+    
     def get_jobs(self):
         
         # Step 1 - Retrieve list of job_ids from the search 
@@ -32,35 +32,48 @@ class Scraper:
         
 
     def get_search_results(self, search_url):
-        raise NotImplementedError("Must be implemented in child class for particular job site.")
+        raise NotImplementedError("Not implemented in Scraper parent class. Please use child class for specific site.")
     
     
     def process_search_results(self, job_ids):
-        raise NotImplementedError("Must be implemented in child class for particular job site.")
+        raise NotImplementedError("Not implemented in Scraper parent class. Please use child class for specific site.")
     
     
     def filter_results(self, results):
-        return [job for job in results if self.job_passes_filter(job)]
+        return [job for job in results if self.__job_passes_filter(job)]
     
     
-    def job_passes_filter(self, job_details):
-        must_contain_terms = self.filters["must_contain_terms"]
+    def __job_passes_filter(self, job_details):
+        must_contain_terms = self.filters.get("must_contain_terms", [])
         for term in must_contain_terms:
             filter_satisfied = term in job_details["title"].lower() or term in job_details["details"].lower()
             if not filter_satisfied:
                 return False
 
-        must_not_contain_terms = self.filters["must_not_contain_terms"]
+        must_not_contain_terms = self.filters.get("must_not_contain_terms", [])
         for term in must_not_contain_terms:
             filter_not_satisfied = term in job_details["title"].lower() or term in job_details["details"].lower()
             if filter_not_satisfied:
                 return False
 
-        must_contain_one_of_terms = self.filters["must_contain_one_of_terms"]
-        filter_satisfied = False
-        for term in must_contain_one_of_terms:
-            if term in job_details["title"].lower() or term in job_details["details"].lower():
-                filter_satisfied = True
-                break
+        must_contain_one_of_terms = self.filters.get("must_contain_one_of_terms", [])
+        if must_contain_one_of_terms:
+            filter_satisfied = False
+            for term in must_contain_one_of_terms:
+                if term in job_details["title"].lower() or term in job_details["details"].lower():
+                    filter_satisfied = True
+                    break
+            
+            if not filter_satisfied:
+                return False
+        
+        return True
 
-        return filter_satisfied
+
+    def __decapitalise_filters(self, filters):
+        if filters:
+            return {
+                key: [item.lower() for item in val] for key, val in filters.items() 
+            }
+        else:
+            return {}
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,4 +1,63 @@
+import pytest
+from functools import partial
+from scraper.scraper import Scraper
 
-# Test filter methods
 
-# Test notimplemented error when scraper is called directly
+class TestScraper:
+    @pytest.fixture
+    def jobs(self):
+        return [
+            {"id": 1, "title": "Developer", "details": "Seeking full time developer"},
+            {"id": 2, "title": "Software Developer", "details": ""},
+            {"id": 3, "title": "Pharmacist", "details": ""},
+            {"id": 4, "title": "Lion Tamer", "details": ""},
+            {"id": 5, "title": "Chef", "details": ""},
+            {"id": 6, "title": "Software Engineer", "details": "Seeking PHP dev"},
+        ]
+
+    def test_filter_results_w_no_filter(self, jobs):
+        scpr = Scraper(["Developer"], ["Brisbane"], "www.job.com", "www.job.com")
+        filtered_results = scpr.filter_results(jobs)
+        assert filtered_results == jobs
+
+    @pytest.mark.parametrize(
+        "filters,expected_result",
+        [
+            (
+                {"must_contain_terms": ["chef"]},
+                [{"id": 5, "title": "Chef", "details": ""}],
+            ),
+            (
+                {"must_contain_terms": ["engineer"], "must_not_contain_terms": ["PHP"]},
+                [],
+            ),
+            (
+                {"must_contain_one_of_terms": ["lion", "chef"]},
+                [
+                    {"id": 4, "title": "Lion Tamer", "details": ""},
+                    {"id": 5, "title": "Chef", "details": ""},
+                ],
+            ),
+        ],
+    )
+    def test_filter_results_w_filter(self, jobs, filters, expected_result):
+        scpr = Scraper(
+            ["Developer"], ["Brisbane"], "www.job.com", "www.job.com", filters=filters
+        )
+        filtered_results = scpr.filter_results(jobs)
+        assert expected_result == filtered_results
+
+    err_scpr = Scraper(["Developer"], ["Brisbane"], "www.job.com", "www.job.com")
+
+    @pytest.mark.parametrize(
+        "scpr_func",
+        [
+            err_scpr.get_jobs,
+            partial(err_scpr.get_search_results, ""),
+            partial(err_scpr.process_search_results, ""),
+        ],
+    )
+    def test_scraper_errors(self, scpr_func):
+        error_msg = "Not implemented in Scraper parent class. Please use child class for specific site."
+        with pytest.raises(NotImplementedError, match=error_msg):
+            scpr_func()
